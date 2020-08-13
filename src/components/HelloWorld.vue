fe926b124e0ffc95b43656e96a0151e697588893
ruslan1996r <dshmc27@gmail.com>
2020-08-13 19:10:09 +0300

    added vuetify icons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,66 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Test: {{test}}</h2>
-    <button @click="testAction(`Hello, User! ${Date.now()}`)">Press</button>
-    <div class="my-2">
-        <v-btn depressed color="error">Error</v-btn>
-      </div>
+  <div id="app">
+    <v-app id="inspire">
+      <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app clipped>
+          <v-list dense>
+            <v-list-item link>
+              <v-list-item-action>
+                <v-icon>mdi-view-dashboard</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Dashboard</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-action>
+                <v-icon>mdi-cog</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Settings</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app clipped-left>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-main>
+          <v-container class="fill-height" fluid>
+            <v-row align="center" justify="center">
+              <v-col class="shrink">
+                <v-tooltip right>
+                  <span>Source</span>
+                </v-tooltip>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-main>
+
+        <v-footer app>
+          <span>&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+      </v-app>
+    </v-app>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+//  <div class="hello">
+//     <h1>{{ msg }}</h1>
+//     <h2>Test: {{test}}</h2>
+//     <button @click="testAction(`Hello, User! ${Date.now()}`)">Press</button>
+//     <div class="my-2">
+//       <v-btn depressed color="error">Error</v-btn>
+//     </div>
+//     <div class="my-2">
+//       <v-btn depressed color="primary">Primary</v-btn>
+//     </div>
+//   </div>
+import { Component, Prop, Vue, Provide } from "vue-property-decorator";
 
 import { botMapper } from "../store/bot";
 
@@ -27,6 +77,7 @@ const Mappers = Vue.extend({
 
 @Component
 export default class HelloWorld extends Mappers {
+  @Provide() drawer: boolean = false;
   @Prop() private msg!: string;
 }
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import 'vuetify/dist/vuetify.css';
 
 Vue.use(Vuetify);
 
-const opts = {};
+const opts = {
+    theme: {
+        dark: true,
+    },
+};
 
 export default new Vuetify(opts);
